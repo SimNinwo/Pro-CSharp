937bbadea9963a99fc85cd57f1466a0f6731e229
Simeon Olorunninwo <simeontoluwanimi@gmail.com>
2020-02-11 10:13:24 +0100

    variable data types

---
+++ BasicDataTypes/BasicDataTypes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalVarDeclarations();
+
+            DefaultDeclarations();
+
+            NewingdataTypes();
+
+            // Wait for user to press the Enter key before shutting down.
+            Console.ReadLine();
+        }
+
+        static void LocalVarDeclarations()
+        {
+            Console.WriteLine("=> Data Declarations:");
+            // Local variables are declared as so:
+            // dataTypes varName = initialvalue;
+            int myInt = 0;
+
+            // You can also declare and assign on two lines
+            string myString;
+            myString = "This is my character data";
+
+            // Declare 3 bools on a single line.
+            bool b1 = true, b2 = false, b3 = b1;
+
+            // Use System.Boolean data type to declare a bool.
+            System.Boolean b4 = false;
+
+            Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}",
+                myInt, myString, b1, b2, b3, b4);
+
+            Console.WriteLine();
+        }
+
+        static void DefaultDeclarations()
+        {
+            Console.WriteLine("=> Default Declarations:");
+            int myInt = default;
+
+            Console.WriteLine("default int: {0}", myInt);
+        }
+
+        static void NewingdataTypes()
+        {
+            Console.WriteLine("=> Using new to create variables:");
+            bool b = new bool();            // Set to false.
+            int i = new int();              // Set to 0.
+            double d = new double();        // Set to 0.
+            DateTime dt = new DateTime();   // Set to 1/1/0001 12:00:00 AM
+            Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
+            Console.WriteLine();
+        }
+    }
+}
